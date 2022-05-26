0fceb85ca1b686e6d1fcd32c764031ac6b8d2d94
RalfBarkow <>
2022-05-26 08:19:20 +0200

    chore(smallest-wiki-revisited/start.js): reformat code & TS2339 [ERROR]
    https://raw.githubusercontent.com/WardCunningham/assets/96f53441a2b4f21c42d1ed971b248dc847a95967/pages/smallest-wiki-revisited/start.js
    
    Configure and run a federated wiki server
    
    deno run --allow-net --allow-read start.js
    Check file:///Users/rgb/.wiki/127.0.0.1/assets/pages/smallest-wiki-revisited/start.js
    error: TS2339 [ERROR]: Property 'getIterator' does not exist on type 'ReadableStream<R>'.
      return res.readable.getIterator();
                          ~~~~~~~~~~~
        at https://deno.land/std@0.79.0/async/pool.ts:45:23

--- pages/smallest-wiki-revisited/start.js
+++ pages/smallest-wiki-revisited/start.js
@@ -3,12 +3,12 @@
 
 
 import { serve } from "https://deno.land/std@0.79.0/http/server.ts";
-import * as host from "./host.js"
+import * as host from "./host.js";
 
 let server = serve({ port: +(Deno.args[0] || 4444) });
 for await (let req of server) {
-  let [domain, port] = (req.headers.get('host')||'localhost').split(':')
-  req.wiki = domain
-  console.log(new Date().toLocaleString(), domain, req.url)
-  host.dispatch(req)
+    let [domain, port] = (req.headers.get('host')||'localhost').split(':');
+    req.wiki = domain;
+    console.log(new Date().toLocaleString(), domain, req.url);
+    host.dispatch(req);
 }
