1761c824ba342eeb1063dc19d285b1d5e3975ffa
RalfBarkow <ralf.barkow@me.com>
2024-03-19 18:10:42 +0100

    fix(handleGraphStream): resolve promise

--- pages/parse-page-paragraphs/handleGraphStream.js
+++ pages/parse-page-paragraphs/handleGraphStream.js
@@ -1,15 +1,16 @@
 import { Graph } from 'https://wardcunningham.github.io/graph/graph.js';
 
 // Function to handle messages received from "SOURCE graph" frame
-function handleGraphStream(message) {
+function handleGraphStream(message, graphPromise) {
     const json = JSON.parse(message.jsonl.trim());
     const graph = new Graph(json.nodes, json.rels);
     console.log("handleGraphStream: ", graph);
 
     // Process the graph data as needed
-    const result = graph.search('match (n:a988355fcbeb4933)');
-    console.log("match: ", result);
+    const result = graph.search('match (n)');
+    console.log("(handleGraphStream) match: ", result);
 
+    // Resolve the promise with the graph instance
+    graphPromise.resolve(graph);
 }
-
-export { handleGraphStream }; // Export the function to make it accessible to other modules
+export { handleGraphStream };
